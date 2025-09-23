1089721d2c36266191447ac51f5eb42f6fe3d83f
parallels <albusfang962@gmail.com>
2025-09-23 16:59:47 -0400

    cart_pole working sim

--- cart_pole/mujoco_cartpole_lqr.py
+++ cart_pole/mujoco_cartpole_lqr.py
@@ -1,5 +1,5 @@
 import debugpy
-debugpy.listen(("0.0.0.0", 5689))   # open debugger server on port 5678
+debugpy.listen(("0.0.0.0", 5689))   
 print("Waiting for debugger attach...")
 debugpy.wait_for_client()
 
@@ -9,14 +9,15 @@ import numpy as np
 from lqr import controllability, lqr
 from cartpole_helper import angle_wrap_around_pi, linearize
 import time
+debug = False
 
 mp = 0.316
 mc = 0.5
-l = 0.2
+l = 0.5
 g = 9.81
 
-Q = np.diag([0.1, 5, 0.1, 0.05])
-R = np.diag([1.0])
+Q = np.diag([1, 2, 0.1, 0.05])
+R = np.diag([5.0])
 
 Umin, Umax = -10.0, 10.0
 
@@ -32,8 +33,9 @@ def main():
     a_cart = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "cart_force")
 
     qpos_adr = model.jnt_qposadr[jnt_id]
+
     # Set starting angle (e.g. 20 degrees)
-    data.qpos[qpos_adr] = np.deg2rad(185)
+    data.qpos[qpos_adr] = np.deg2rad(200)
     data.qpos[model.jnt_qposadr[j_slider]] = 0.5
     data.qvel[model.jnt_dofadr[jnt_id]] = 0.0
     data.qvel[model.jnt_dofadr[j_slider]] = 0.0
@@ -52,20 +54,20 @@ def main():
         print_timer = 0.0
 
         while viewer.is_running() and data.time < sim_end:
-        
-        # for k in range(steps):
-            # Read joint states directly (simpler than sensors)
+            # Read state values
             x      = data.qpos[model.jnt_qposadr[j_slider]]
             theta  = data.qpos[model.jnt_qposadr[jnt_id]]
             xdot   = data.qvel[model.jnt_dofadr[j_slider]]
             angular_vel   = data.qvel[model.jnt_dofadr[jnt_id]]
 
             current_state = np.array([x, theta, xdot, angular_vel])
+
             # Calculate error term
             e = np.array(current_state - z_eq)
             e[1] = angle_wrap_around_pi(e[1])
-            # print(np.array(K).shape)
-            # print(e.shape)
+            if debug:
+                print(np.array(K).shape)
+                print(e.shape)
 
             u_in = float(-(K @ e).squeeze())
             
@@ -73,19 +75,16 @@ def main():
             u = max(Umin, min(Umax, u_in))
             data.ctrl[a_cart] = u
 
-                    # Print every 0.5 seconds of sim time
+            # Print every 0.5 seconds of sim time
             if data.time - print_timer >= 0.5:
                 print(f"t={data.time:.2f} | x={x:.3f}, theta_err={e[1]:.3f}, "
                     f"xdot={xdot:.3f}, angular_vel={angular_vel:.3f}, u={u:.3f}")
+            
             # Step simulation
             mujoco.mj_step(model, data)
             viewer.sync()
             next_frame += dt
             time.sleep(max(0.0, next_frame - time.time()))
-            # if k % int(0.5 / dt) == 0:
-            #     print(
-            #         f"t={k*dt:5.2f}  x={x:+.3f}  th_err={e[1]:+.3f}  xdot={xdot:+.3f}  tdot={angular_vel:+.3f}  u={u:+.2f}"
-            #     )
 
 if __name__ == "__main__":
     main()
--- cart_pole/mujoco_test_actuator.py
+++ cart_pole/mujoco_test_actuator.py
@@ -5,8 +5,6 @@ from lqr import controllability, lqr
 from cartpole_helper import angle_wrap_around_pi, linearize
 import time
 
-
-
 def main():
     model = mujoco.MjModel.from_xml_path("cartpole.xml")
     data  = mujoco.MjData(model)
@@ -17,19 +15,34 @@ def main():
     j_slider = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_JOINT, "slider")
     a_cart = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, "cart_force")
 
-    qpos_adr = model.jnt_qposadr[jnt_id]
-    # Set starting angle (e.g. 20 degrees)
-    data.qpos[qpos_adr] = np.deg2rad(185)
-    data.qpos[model.jnt_qposadr[j_slider]] = 0.5
-    data.qvel[model.jnt_dofadr[jnt_id]] = 0.0
-    data.qvel[model.jnt_dofadr[j_slider]] = 0.0
+    # qpos_adr = model.jnt_qposadr[jnt_id]
+    # # Set starting angle (e.g. 20 degrees)
+    # data.qpos[qpos_adr] = np.deg2rad(185)
+    # data.qpos[model.jnt_qposadr[j_slider]] = 0.0
+    # data.qvel[model.jnt_dofadr[jnt_id]] = 0.0
+    # data.qvel[model.jnt_dofadr[j_slider]] = 0.0
 
     
     
+        # Launch interactive viewer
     with mujoco.viewer.launch_passive(model, data) as viewer:
-        data.ctrl[a_cart] = +1.0
-        for _ in range(50): mujoco.mj_step(model, data)
-        print("Δx after +1N pulse:", data.qpos[model.jnt_qposadr[j_slider]] - 0.0)
+        start = time.time()
+        while viewer.is_running() and time.time() - start < 10:  # run 10 seconds
+            step_start = time.time()
+
+            # Apply control
+            data.ctrl[a_cart] = +50.0
+
+            # Step the simulation
+            mujoco.mj_step(model, data)
+
+            # Sync the viewer so it redraws
+            viewer.sync()
+
+            # Keep real time
+            time_until_next_step = dt - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
 
 
 if __name__ == "__main__":
